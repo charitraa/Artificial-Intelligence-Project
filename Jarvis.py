c3b3ac7582e95ab3d6fbab4bcf09455ec3fdfc2b
Your Name <you@example.com>
2023-08-13 07:38:39 +0545

    upgrade

--- Jarvis.py
+++ Jarvis.py
@@ -5,15 +5,20 @@ import wikipedia
 import webbrowser
 import os
 import smtplib
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from time import sleep
 
-newVoiceRate = 160
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-engine.setProperty('rate', newVoiceRate)
+chrome_options = Options()
+chrome_options.add_argument('--log-level=3')
 
 
 def speak(audio):
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 170)
     engine.say(audio)
     engine.runAndWait()
 
@@ -33,12 +38,11 @@ def wishMe():
 
 
 def takeCommand():
-
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("i am Listening sir ...")
         r.pause_threshold = 1
-        audio = r.listen(source)
+        audio = r.listen(source, 0, 8)
 
     try:
         print("Recognizing...")
@@ -56,16 +60,15 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
+    server.login('stharabi9862187405@gmail.com', 'SUSMITASHRESTHA')
+    server.sendmail('stharabi9862187405@gmail.com', to, content)
     server.close()
 
 
 if __name__ == "__main__":
     wishMe()
     while True:
-
-        query = takeCommand().lower()  # type: ignore
+        query = str(takeCommand()).lower()
 
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
@@ -107,4 +110,10 @@ if __name__ == "__main__":
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend harry chai. I am not able to send this email")
+                speak("Sorry sir i can't send email")
+
+        elif 'shutdown' in query:
+            os.system("shutdown /s /t 1")
+
+        elif 'restart' in query:
+            os.system("shutdown /r /t 1")
